a772181211f0484248a74c56d5d1cea131d1b64f
Rysul Hasan Hredoy <162629392+Rysulhasan@users.noreply.github.com>
2025-04-10 14:16:33 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,17 +195,6 @@ fetchData("projects").then(data => {
 // <!-- contact section ends -->
 
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/64f86039a91e863a5c11f729/1h9l3fofu';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-
-
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
     origin: 'top',
